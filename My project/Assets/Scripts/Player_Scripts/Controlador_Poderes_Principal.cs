27847fec9469a7997b0eefbcc9b68abbfa00a015
josebarrancas <a21300623@ceti.mx>
2026-05-28 11:39:07 -0600

    impulso

--- My project/Assets/Scripts/Player_Scripts/Controlador_Poderes_Principal.cs
+++ My project/Assets/Scripts/Player_Scripts/Controlador_Poderes_Principal.cs
@@ -200,6 +200,22 @@ public class Controlador_Poderes_Principal : MonoBehaviour
                 RegistrarEventoPoder(rawNombre);
             }
         }
+        // -- IMPULSO --
+        else if (nombre == "impulso")
+        {
+            ImpulsoPower script = GetComponent<ImpulsoPower>();
+            if (script != null && script.cargasRestantes > 0)
+            {
+                // Leemos las teclas de dirección que el jugador está presionando
+                float inputX = Input.GetAxisRaw("Horizontal");
+                float inputY = Input.GetAxisRaw("Vertical");
+
+                Vector2 direccionApuntada = new Vector2(inputX, inputY);
+
+                script.EjecutarImpulso(direccionApuntada);
+                RegistrarEventoPoder(rawNombre);
+            }
+        }
         // -- IMÁN / DESPLAZADOR --
         else if (nombre == "iman" || nombre == "imán" || nombre == "desplazador")
         {
